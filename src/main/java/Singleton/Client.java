8379b2d58b63d573009162ef167af972f0abcce0
cergiyko <sergiymalashenko@gmail.com>
2023-03-05 21:29:47 +0200

    Create Singleton

---
+++ src/main/java/Singleton/Client.java
@@ -0,0 +1,10 @@
+package Singleton;
+
+
+public class Client {
+    public static void main( String[] args ){
+        Singleton singletonOne = Singleton.getInstance("Singleton One");
+        Singleton singletonNext = Singleton.getInstance("Singleton Next");
+        System.out.println(singletonOne);
+        System.out.println(singletonNext);
+   }}
---
+++ src/main/java/Singleton/Singleton.java
@@ -0,0 +1,36 @@
+package Singleton;
+
+import java.util.Objects;
+
+public final class Singleton {
+    private static Singleton instance;
+    private String name;
+
+    private Singleton(String name){
+        this.name = name;
+    }
+    public static Singleton getInstance(String name){
+        if (instance==null){
+            instance = new Singleton(name);}
+            return  instance;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Singleton singleton = (Singleton) o;
+        return Objects.equals(name, singleton.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Singleton{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
